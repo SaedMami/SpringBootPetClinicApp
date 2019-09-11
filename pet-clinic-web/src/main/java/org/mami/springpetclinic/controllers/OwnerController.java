3ff300ecc8398470c1f3e004e3c80b2abdd90623
Saed Mami <saed.mami@gmail.com>
2019-09-11 22:05:45 +0100

    Listing the owners on the Owners index page

--- pet-clinic-web/src/main/java/org/mami/springpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/org/mami/springpetclinic/controllers/OwnerController.java
@@ -1,13 +1,25 @@
 package org.mami.springpetclinic.controllers;
 
+import org.mami.springpetclinic.services.crud.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/owner")
 public class OwnerController {
+    private OwnerService ownerService;
+
+    @Autowired
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+
     @RequestMapping("")
-    public String listOwners() {
+    public String listOwners(Model model) {
+        model.addAttribute("owners", this.ownerService.findAll());
         return "owner/index";
     }
 }
