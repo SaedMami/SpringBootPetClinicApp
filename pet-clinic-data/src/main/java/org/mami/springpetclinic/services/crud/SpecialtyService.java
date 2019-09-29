2970c13bed01d4d5942d72c167d1f52771fc9906
Saed Mami <saed.mami@gmail.com>
2019-09-29 15:34:25 +0100

    Add Specialty Service and its Map Implementation

---
+++ pet-clinic-data/src/main/java/org/mami/springpetclinic/services/crud/SpecialtyService.java
@@ -0,0 +1,6 @@
+package org.mami.springpetclinic.services.crud;
+
+import org.mami.springpetclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
---
+++ pet-clinic-data/src/main/java/org/mami/springpetclinic/services/crud/map/SpecialtyServiceMap.java
@@ -0,0 +1,9 @@
+package org.mami.springpetclinic.services.crud.map;
+
+import org.mami.springpetclinic.model.Specialty;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SpecialtyServiceMap extends AbstractMapCrudService<Specialty> {
+
+}
