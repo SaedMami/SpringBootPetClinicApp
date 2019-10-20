e7aefe0f3be4c71edd2142f52e9d5974f69a7dbc
Saed Mami <saed.mami@gmail.com>
2019-10-20 22:29:01 +0100

    Implement the Map CRUD Service for Visit

--- pet-clinic-data/src/main/java/org/mami/springpetclinic/services/crud/map/VisitServiceMap.java
+++ pet-clinic-data/src/main/java/org/mami/springpetclinic/services/crud/map/VisitServiceMap.java
@@ -6,4 +6,12 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class VisitServiceMap extends AbstractMapCrudService<Visit> implements VisitService {
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getId() == null || visit.getPet().getOwner() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+
+        return super.save(visit);
+    }
 }
